57eb589dc1833328382ee4a3ade7735fb3dcc0a2
rafael01gx <rafael_junio_moraes@hotmail.com>
2026-01-06 01:51:03 -0300

    Ref: Dockerfile

--- OrchestratorService/src/main/java/br/com/microservices/orchestrated/OrchestratorService/config/kafka/KafkaConfig.java
+++ OrchestratorService/src/main/java/br/com/microservices/orchestrated/OrchestratorService/config/kafka/KafkaConfig.java
@@ -7,7 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin.NewTopics;
 
